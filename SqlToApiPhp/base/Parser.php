3c2f154e0f9119af71af25e03ebd100d3c8c70b4
Dmitriy Haralson <diharalson@gmail.com>
2018-10-01 21:07:23 +0300

    Parser can fetch api path from query

--- SqlToApiPhp/base/Parser.php
+++ SqlToApiPhp/base/Parser.php
@@ -2,6 +2,7 @@
 
 namespace SqlToApiPhp\base;
 
+use PhpMyAdmin\SqlParser\Parser as PhpMyAdminParser;
 
 /**
  * Class Parser
@@ -9,7 +10,35 @@ namespace SqlToApiPhp\base;
  */
 class Parser {
 
-    public function __construct() {
+    /** @var PhpMyAdminParser */
+    private $parser;
 
+    /**
+     * Parser constructor.
+     *
+     * @param string $query
+     */
+    public function __construct(string $query) {
+        $this->parser = new PhpMyAdminParser($query);
     }
+
+    /**
+     * @return string
+     */
+    public function getApiRoute(): string {
+        $statement = $this->parser->statements[0];
+        $fromStatement = $statement->from[0];
+
+        $path = '';
+
+        if ($fromStatement->database !== null) {
+            $path .= '/' . $fromStatement->database;
+        }
+        if ($fromStatement->table !== null) {
+            $path .= '/' . $fromStatement->table;
+        }
+
+        return $path;
+    }
+
 }
