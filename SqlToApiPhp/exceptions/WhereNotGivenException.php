0f469fcc066aa553c5a1749a37a3bd31f5c9c89f
Dmitriy Haralson <diharalson@gmail.com>
2018-10-01 21:35:48 +0300

    Get the request parameters from the 'WHERE' clause

---
+++ SqlToApiPhp/exceptions/WhereNotGivenException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SqlToApiPhp\exceptions;
+
+
+/**
+ * Class WhereNotGivenException
+ * @package SqlToApiPhp\exceptions
+ */
+class WhereNotGivenException extends \Exception {
+
+}
